61fa7412f1215881ffab20b1ae90b21841e01918
PSNapier <46052087+PSNapier@users.noreply.github.com>
2024-04-08 07:25:23 -0500

    feat: populated dictionary

--- src/js/dictionaries.js
+++ src/js/dictionaries.js
@@ -1,7 +1,6 @@
 // DICTIONARIES
 
 var dictionary = {
-	// species
 	species: [
 		'velox',
 		'hexin',
@@ -57,4 +56,157 @@ var dictionary = {
 		['ziemel', 'minkins'],
 		['ziemel', 'vilkren'],
 	],
+	rank: [
+		'runt',
+		'omega',
+		'beta',
+		'alpha',
+	],
+	build: {
+		common: [
+			'natural',
+		],
+		uncommon: [
+			'sphynx', 
+			'satin', 
+			'ridgeback',
+		],
+		rare: [
+			'leo',
+		],
+	},
+	mutations: {
+		random: [
+			'albino',
+			'celestial',
+			'chimera',
+			'deaf and blind',
+			'sclera',
+			'polycoria',
+		],
+		inbred: [
+			'amputated',
+			'angel\'s gift',
+			'polymelia',
+			'harlequin',
+			'optical',
+			'zombified',
+		]
+	},
+	coatColours: {
+		common: [
+			'daybreak',
+		],
+		uncommon: [
+			'ocean',
+		],
+		rare: [
+			'midnight',
+		],
+	},
+	markings: {
+		common: [
+			['accents','acc'],
+			['blanket','bla'],
+			['glitz','gli'],
+			['hood','hod'],
+			['points','pts'],
+			['sable','sab'],
+			['masked','mas'],
+		],
+		uncommon: [
+			['dual tone','duo'],
+			['merle','mrl'],
+			['spectral','spe'],
+			['snowstorm','sno'],
+			['tabby','tab'],
+			['wilddog','wdg'],
+		],
+		rare: [
+			['aura','aur'],
+			['calico','cal'],
+			['defier','def'],
+			['piebald','pie'],
+			['shine','shi'],
+		],
+		unique: [
+			['crystal','cry'],
+			['malachite','mal'],
+			['meteor rain','mtr'],
+			['reversal','rev'],
+			['tribal','tri'],
+			['void','voi'],
+		],
+	},
+	hereditaryTraits: {
+		common: [
+			'hunter',
+			'feathery flyer',
+			'bone crusher',
+			'fisher',
+			'explorer',
+			'camper',
+		],
+		uncommon: [
+			'meat hoarder',
+			'herbal hoarder',
+			'medic',
+			'fellowship I',
+			'skull breaker',
+		],
+		rare: [
+			'runebook searcher',
+			'ancient runes',
+			'strength addiction',
+			'mile runner',
+			'guardian protector',
+			'warm heart',
+		],
+		unique: [
+			'velox DNA I',
+			'hexin DNA I',
+			'enquisitors DNA I',
+			'arcid DNA I',
+			'mykron DNA I',
+			'zinner DNA I',
+			'zephies DNA I',
+			'minkin DNA I',
+			'funia DNA I',
+			'vilkren DNA I',
+		],
+	},
+	physicalTraits: {
+		ears: [
+			'folded ears',
+			'standard ears',
+			'rounded ears',
+			'pointed ears',
+			'tipsy ears',
+			'furred ears',
+			'horned ears',
+		],
+		tail: [
+			'curled tail',
+			'half tail',
+			'double tail',
+			'kyuubi tail',
+			'fin tail',
+			'equus tail',
+			'leopard tail',
+			'tuft tail',
+		],
+		bonus: [
+			'fangs',
+			'horns',
+			'spikes',
+			'tusks',
+			'quills',
+			'elemental',
+			'theri claws',
+			'fins',
+			'scales / armor plates',
+			'antennas',
+			'dragon vines',
+		],
+	},
 }
--- src/js/input.js
+++ src/js/input.js
@@ -1,5 +1,13 @@
 // INPUT
 
+// auto populate parents
+// go through dictionary and populate input UI based upon available options
+
+// create a select menu based on dictionary.species and add it to parent1 div
+for (let i = 0; i < dictionary.species.length; i++) {
+	
+}
+
 const parent1 = {
 	geno: '',
 	rank: 'runt',
