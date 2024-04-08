998aa6a3a6c91552f52f221089b08e23024a52da
PSNapier <46052087+PSNapier@users.noreply.github.com>
2024-04-08 07:58:21 -0500

    feat: start of auto populating inputs

--- src/js/input.js
+++ src/js/input.js
@@ -3,10 +3,23 @@
 // auto populate parents
 // go through dictionary and populate input UI based upon available options
 
-// create a select menu based on dictionary.species and add it to parent1 div
-for (let i = 0; i < dictionary.species.length; i++) {
-	
+function addSelect(id, array) { // array = ['string', 'string,'...]
+	['parent1','parent2'].forEach((parent) => {
+		let select = document.createElement('select');
+		select.id = `${parent}${id}`;
+		select.classList .add('text-neutral-900');
+		document.getElementById(parent).appendChild(select);
+		for (let i = 0; i < array.length; i++) {
+			let option = document.createElement('option');
+			option.value = array[i].replace(' ', '-');
+			option.text = array[i].capitalizeStr();
+			select.appendChild(option);
+		}
+	})
 }
+
+// create a select menu based on dictionary.species and add it to parent1 div
+addSelect('species', dictionary.species);
 
 const parent1 = {
 	geno: '',
